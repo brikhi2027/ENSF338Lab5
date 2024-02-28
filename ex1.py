f6390571947d57a5a9abd85945a5f2245f280679
Julien Rabino <julienmescha.rabino@ucalgary.ca>
2024-02-27 20:39:20 -0700

    finished 1

--- ex1.py
+++ ex1.py
@@ -1,5 +1,17 @@
 import sys
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
 
 if len(sys.argv) != 2:
     sys.exit(1)
@@ -7,24 +19,21 @@ if len(sys.argv) != 2:
 operations_list = ['+','-','*','/']
 expression = sys.argv[1]
 
-
-stack = []
-i = 0
-while i < len(expression):
-    if expression[i] == '(':
-        i += 1
-        new_exp = ''
-        while i < len(expression) and expression[i] != ')':
-            if expression[i]== '(':
-                break
-            new_exp += expression[i]
-            i += 1
-        stack.append(new_exp)
-    i += 1
-
-print(stack)
-
+stack = Stack()
+for char in expression:
+    if char == '(':
+        continue
+    elif char in operations_list:
+        stack.push(char)
+    elif char.isdigit():
+        stack.push(char)
+    elif char == ')':
+        num2 = stack.pop()
+        num1 = stack.pop()
+        operation = stack.pop()
+        new_expression = str(num1) + ' ' + operation + ' ' + str(num2)
+        calculate = int(eval(new_expression))
+        stack.push(calculate) 
 
 
-
-
+print("Result:", stack.pop())
