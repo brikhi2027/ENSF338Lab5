a1177bd82c5b0144466f1895b1d283723aacfbc0
AnastasiaZ04 <anastasia.zaharia@ucalgary.ca>
2024-03-05 12:22:49 -0700

    fixes to 5.2, finished 5.3

--- ex5.py
+++ ex5.py
@@ -56,9 +56,11 @@ class ListNode:
         return str(self._value)
 
 class ListQueue:
-    def __init__(self):
+    def __init__(self, capacity):
         self._head = None
         self._tail = None
+        self._size = 0
+        self._capacity = capacity
     def enqueue(self, value):
         new_node = ListNode(value)
         if self._head is None:
@@ -66,17 +68,22 @@ class ListQueue:
             self._tail = self._head
             self._head._next = self._head # set the next pointer of the only node to point to itself
             print('enqueue', value)
-        else:
+        elif self._size < self._capacity:
             self._tail._next = new_node
             print('enqueue', value)
-        self._tail = new_node
-        self._tail._next = self._head
+            self._tail = new_node
+            self._tail._next = self._head
+        else:
+            print('enqueue None')
+        self._size += 1
+
     def dequeue(self):
         if self._head is None:
             print('dequeue None')
             return
         temp = self._head._value
-        if self._head == self._tail:
+
+        if (self._head == self._tail) and (self._head._next == None):
             self._head = None
             self._tail = None
             print('dequeue', temp)
@@ -85,6 +92,7 @@ class ListQueue:
             self._tail._next = self._head
             print('dequeue', temp)
         return temp
+    
     def peek(self):
         if self._head is None:
             print('peek None')
@@ -95,6 +103,44 @@ class ListQueue:
         return
                     
       
+# 3. Generate a list of 40 operations, together with expected output, that can
+# be used to test correctness of implementation. [0.2 pts]
 
+def test_implementation(queue):
+    my_queue.dequeue() # test 1: dequeueing from empty queue
+    print('Expected: dequeue None\n')
 
+    my_queue.peek() # test 2: peeking into empty queue
+    print('Expected: peek None\n')
+
+    my_queue.enqueue(1) # test 3: enqueueing into empty queue
+    print('Expected: enqueue 1\n')
+    
+    # tests 4 - 20: some regular enqueue operations
+    for i in range(2, 19):
+        my_queue.enqueue(i) # a regular enqueue operation
+        expected_output = 'enqueue ' + str(i)
+        print('Expected:',expected_output,'\n')
+
+    my_queue.enqueue(2) # test 21: enqueueing into full queue
+    print('Expected Output: enqueue None\n')
+    
+    # tests 22 - 39: some regular dequeuing and peek operations
+    k = 1
+    for i in range(0, 9):
+        my_queue.peek()
+        expected_output2 = 'peek ' + str(k)
+        print('Expected:',expected_output2,'\n')
+
+        my_queue.dequeue() # a regular dequeue operation
+        expected_output = 'dequeue ' + str(k)
+        print('Expected:',expected_output, '\n')
+        k += 1
+    
+    my_queue.dequeue() # test 40: a regular dequeue operation
+    expected_output = 'dequeue 10'
+    print('Expected:',expected_output, '\n')
+
+my_queue = ListQueue(18)
+test_implementation(my_queue)
 
